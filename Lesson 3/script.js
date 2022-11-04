29fe89f9043117ed69bf6ef5e12a0bf3c8f676f8
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2022-11-04 22:26:46 +0200

    Add files via upload

---
+++ Lesson 3/script.js
@@ -0,0 +1,39 @@
+//Переписати код нижче з використанням конструкції switch…case
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if(numOrStr === null) {
+// console.log('ви скасували')
+// } else if( numOrStr.trim() === '' ) {
+// console.log('Empty String');
+// } else if ( isNaN( +numOrStr ) ) {
+// console.log(' number is Ba_NaN')
+// } else {
+// console.log('OK!')
+// }
+
+
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+switch(numOrStr){
+  case null:
+    console.log('ви скасували');
+    break;
+  
+  default:
+    switch(numOrStr.trim()){
+      case (''):
+        console.log('Empty String');
+        break;
+      
+    }
+    switch(isNaN( +numOrStr )){
+      case true:
+      console.log( 'number is Ba_NaN');
+      break;
+      default:
+        console.log('OK!');
+    }
+}
+
