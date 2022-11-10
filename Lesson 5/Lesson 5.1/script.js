53a42fc838c9cd056b7726bb85a84f3d94e1babc
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2022-11-10 20:18:02 +0200

    Add files via upload

---
+++ Lesson 5/Lesson 5.1/script.js
@@ -0,0 +1,40 @@
+//Створити масив, довжину та елементи якого задає користувач.
+//У міру змін виводити вміст масиву на сторінку.
+
+// debugger;
+let userArr = [];
+let arrLength;
+let arrLengthCheck = false;
+
+
+while(!arrLengthCheck) {
+
+    arrLength = parseInt(prompt(`Введите кол-во элементов массива данных`));
+    alert(`Ваш массив будет содержать ${arrLength} элементов`);
+
+    if ( !arrLength ) {
+            alert('Вы должны ввести значение');
+    } else if( arrLength < 4 ) {
+        alert('Массив должен содержать не менее 4х элементов');
+    } else {
+        arrLengthCheck = true;
+    }
+    
+}
+
+
+do {
+
+    let arrElement = prompt(`Введите елемент`);
+    userArr.push(arrElement);
+    
+} while ( userArr.length !== arrLength );
+alert(`Ваш массив теперь выглядит так - [${userArr}]`);
+
+//Відсортувати масив за зростанням.
+userArr.sort();
+alert(`Ваш массив отсортирован по возрастанию [${userArr}]`);
+//Видалити елементи з масиву з 2 по 4 (включно!).
+userArr.splice(1, 3);
+alert(`Из массива удалены элементы с 2 по 4й включительно. Массив выглядит вот так - [${userArr}]`);
+
