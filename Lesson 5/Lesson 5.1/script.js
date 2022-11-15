4c2592252804885c5c023adf7d2cd3d259787643
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2022-11-15 12:54:17 +0200

    Update script.js

--- Lesson 5/Lesson 5.1/script.js
+++ Lesson 5/Lesson 5.1/script.js
@@ -4,24 +4,21 @@
 // debugger;
 let userArr = [];
 let arrLength;
-let arrLengthCheck = false;
 
 //Получаем длинну массива
-while(!arrLengthCheck) {
+do {
 
     arrLength = parseInt(prompt(`Введите кол-во элементов массива данных`));
     
-
     if ( !arrLength ) {
             alert('Вы должны ввести значение');
     } else if( arrLength < 4 ) {
         alert('Массив должен содержать не менее 4х элементов');
     } else {
-        arrLengthCheck = true;
         alert(`Ваш массив будет содержать ${arrLength} элементов`);
     }
     
-}
+} while( (arrLength < 4) || !arrLength );
 
 //Получаем каждый элемент
 let arrElement;
