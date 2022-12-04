05bed9dc282aa715266880dd18b0a38d350e1b2c
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2022-12-04 21:13:47 +0200

    Update script.js

--- Lesson 8/Lesson 8.5/script.js
+++ Lesson 8/Lesson 8.5/script.js
@@ -5,3 +5,29 @@
 
 
 // newList = copy(list, function(value){ return value*10; })
+let someArr = [44, 15, 'Asus', false, function someFunc() {}, 10, 'Hator', true, 20];
+
+
+function copyArr(arr, func) {
+
+    let myArrCopy = arr.slice();
+    
+    if( typeof func === 'function'){
+
+        for ( let i = 0; i < myArrCopy.length; i++) {
+
+            if ( typeof myArrCopy[i] === 'number') {
+
+                myArrCopy[i] = multiplyValue(myArrCopy[i]);
+                
+                
+            }
+        }
+    }
+    return myArrCopy;
+}
+
+function multiplyValue(value){
+    return value *= 10;
+}
+console.log(copyArr(someArr, multiplyValue));
