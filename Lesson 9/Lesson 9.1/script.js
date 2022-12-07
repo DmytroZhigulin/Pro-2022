a1d3c9e6a648b93667c5102bf99c2959229d547f
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2022-12-07 11:04:18 +0200

    Update script.js

--- Lesson 9/Lesson 9.1/script.js
+++ Lesson 9/Lesson 9.1/script.js
@@ -13,20 +13,27 @@
 let digitArr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
 // Знайти суму та кількість позитивних елементів.
+// Знайти добуток позитивних елементів.
 // Сума
 let sum = 0;
+let total = 1;
 let positiveDigits = digitArr
     .filter(function(digit) {
         return digit > 0;
     })
-//console.log(positiveDigits);
+// console.log(positiveDigits);
 
 // Кількість
 positiveDigits.forEach(function(digit) {
     sum += digit;
 })
+// Добуток
+positiveDigits.forEach(function(digit) {
+    total *= digit;
+})
 console.log(`Кількість позитивних елементів массиву = ${positiveDigits.length}`);
 console.log(`Сума позитивних елементів массиву = ${sum}`);
+console.log(`Добуток позитивних елементів массиву = ${total}`);
 
 
 // Знайти максимальний елемент масиву та його порядковий номер.
@@ -37,14 +44,16 @@ let maxNumber = (a, b) => {
 let minNumber = (a, b) => {
         return a < b ? a : b
     };
+let maxIn = digitArr.indexOf(digitArr.reduce(maxNumber));
+let minIn = digitArr.indexOf(digitArr.reduce(minNumber));
 
-console.log(`Максимальний елемент масиву ${digitArr.reduce(maxNumber)}`);// Макс
-console.log(`Мінімальний елемент масиву ${digitArr.reduce(minNumber)}`);// Мін
+console.log(`Максимальний елемент масиву ${digitArr.reduce(maxNumber)}, а його індекс ${maxIn}`);// Макс
+console.log(`Мінімальний елемент масиву ${digitArr.reduce(minNumber)}, а його індекс ${minIn}`);// Мін
 
 // Визначити кількість негативних елементів.
 let negativeDigits = digitArr
     .filter(function(digit) {
         return digit < 0;
     })
-//console.log(negativeDigits);
+// console.log(negativeDigits);
 console.log(`Кількість негативних елементів массиву = ${negativeDigits.length}`);
