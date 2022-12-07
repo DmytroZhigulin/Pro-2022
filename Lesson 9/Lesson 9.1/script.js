713b349d0cc59c9aa65ff5ada8790053af1414a8
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2022-12-07 10:18:12 +0200

    Update script.js

--- Lesson 9/Lesson 9.1/script.js
+++ Lesson 9/Lesson 9.1/script.js
@@ -9,3 +9,41 @@
 // Знайти добуток позитивних елементів.
 // Знайти найбільший серед елементів масиву, ост альні обнулити.
 
+
+let digitArr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+
+// Знайти суму та кількість позитивних елементів.
+// Сума
+let sum = 0;
+let positiveDigits = digitArr
+    .filter(function(digit) {
+        return digit > 0;
+    })
+console.log(positiveDigits);
+console.log(`Кількість позитивних елементів массиву = ${positiveDigits.length}`);
+// Кількість
+positiveDigits.forEach(function(digit) {
+    sum += digit;
+})
+console.log(`Сума позитивних елементів массиву = ${sum}`);
+
+
+// Знайти максимальний елемент масиву та його порядковий номер.
+// Знайти мінімальний елемент масиву та його порядковий номер.
+let maxNumber = (a, b) => {
+    return a > b ? a : b;
+};
+let minNumber = (a, b) => {
+        return a < b ? a : b
+    };
+
+console.log(`Максимальний елемент масиву ${digitArr.reduce(maxNumber)}`);// Макс
+console.log(`Мінімальний елемент масиву ${digitArr.reduce(minNumber)}`);// Мін
+
+// Визначити кількість негативних елементів.
+let negativeDigits = digitArr
+    .filter(function(digit) {
+        return digit < 0;
+    })
+console.log(negativeDigits);
+console.log(`Кількість негативних елементів массиву = ${negativeDigits.length}`);
