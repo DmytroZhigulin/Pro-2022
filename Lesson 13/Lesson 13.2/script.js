9f41f19d71b1c66552e219fca45d49e95c4561e0
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-01-03 11:00:08 +0200

    Update script.js

--- Lesson 13/Lesson 13.2/script.js
+++ Lesson 13/Lesson 13.2/script.js
@@ -14,3 +14,59 @@
 
 // p = new SuperMath();
 // p.check(obj); // --> no p.input() -> 3 prompt -> рахує
+
+class SuperMath{
+
+	constructor() {
+		this.input();
+	}
+
+	input() {
+		do{
+			this.x = +prompt(`Enter x`);
+		} while(isNaN(this.x));
+
+		do{
+			this.y = +prompt(`Enter y`);
+		} while(isNaN(this.y));
+
+		do{
+			this.sign = prompt(`Enter sign: ${this.getOperations()}`);
+		} while(!mathOperation[this.sign]);
+
+		return this;
+	}
+
+    check() {
+		let userApproved = confirm(`Do you want make operation ${this.x} ${this.sign} ${this.y}?`);
+
+		return userApproved ?  this.calc() : this.input().check();
+	}
+
+    calc() {
+        let calculate = mathOperation[this.sign];
+
+        return calculate(this.x, this.y);
+    }
+
+	getOperations() {
+		let mathSigns = [];
+		for(let key in mathOperation){
+			mathSigns.push(key);
+		}
+
+		return mathSigns.join(`, `);
+	}
+}
+
+let mathOperation = {
+	'+': ((x,y) =>  { return x+y }),
+	'-': ((x,y) => { return x-y }),
+	'/': ((x,y) => { return x/y }),
+    '*': ((x,y) => { return x*y }),
+    '%': ((x,y) => { return x%y }),
+}
+
+
+let superMathAction = new SuperMath();
+console.log(superMathAction.check());
