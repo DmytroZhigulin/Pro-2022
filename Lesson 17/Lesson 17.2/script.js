b7658ba394bf0095296ad4898a7536537c48f2e8
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-01-19 21:54:33 +0200

    Add files via upload

--- Lesson 17/Lesson 17.2/script.js
+++ Lesson 17/Lesson 17.2/script.js
@@ -6,4 +6,57 @@
 
 // input для выбора цвета. При его изменении меняется цвет фигуры
 
+//создаем все элементы на странице
+function createAllElements() {
+    const allWrap = document.querySelector('.wrap');
+    const figureWrap = document.createElement('div');
+    const figure = document.createElement('div');
+    const options = document.createElement('div');
+    const select = document.createElement('select');
 
+    let figures = ['square', 'rectangle', 'circle'];
+    figures.forEach((item, index) => {
+        const selectOption = document.createElement('option');
+        selectOption.setAttribute('id', `${index + 1}`);
+        selectOption.innerHTML = `${item}`;
+        select.append(selectOption);
+    });
+
+    const inputColor = document.createElement('input');
+    
+
+    figureWrap.classList.add('figure-wrap');
+    figureWrap.setAttribute('id', 'figure-wrap');
+    options.classList.add('options');
+    select.setAttribute('id', 'select-color');
+    inputColor.setAttribute('type', 'color');
+
+    allWrap.append(figureWrap);
+    figureWrap.append(figure);
+    allWrap.append(options);
+    options.append(select);
+    options.append(inputColor);
+    changeFigure();
+}
+createAllElements();
+
+//отлавливаем изменение значения input
+function changeFigure() {
+    let myFigure = document.getElementById('figure-wrap').firstChild;
+    let defFugure = document.getElementById('select-color');
+    myFigure.id = defFugure.value;
+    document.querySelector("select").addEventListener('change', function (e) {
+        myFigure.id = `${e.target.value}`;
+      });
+    changeColor(myFigure);  
+}
+
+
+//меняем цвет
+function changeColor(figure) {
+    document.querySelector("input").addEventListener('change', function (e) {
+        figure.style.background = e.target.value;
+        console.log(figure);
+        console.log(e.target.value);
+    });
+}
