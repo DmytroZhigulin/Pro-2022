511f76f27bce108a6c15cbd19abb6f8d0bcf1c25
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-01-21 17:54:21 +0200

    Update script.js

--- Lesson 17/Lesson 17.1/script.js
+++ Lesson 17/Lesson 17.1/script.js
@@ -1,2 +1,23 @@
 // На сторінці є дві кнопки. - переадресовується на інший сайт (за раніше введеним посиланням). 
 // Реалізувати перевірку на http/https. Якщо протокол не вказаний - додаємо
+
+let firstBtn = document.querySelector('.first__button');
+let secBtn = document.querySelector('.sec__button');
+
+
+function linkCheck(link) {
+    if (link.indexOf('https://') != -1 || link.indexOf('http://') != -1) {
+        return true;
+    }
+    return false;
+}
+
+firstBtn.addEventListener('click', () => {
+    let input1 = document.getElementById('1_link');
+    linkCheck(input1.value) ? window.open(input1.value) : window.open(`https://${input1.value}`);
+});
+
+secBtn.addEventListener('click', () => {
+    let input2 = document.getElementById('2_link');
+    linkCheck(input2.value) ? window.open(input2.value) : window.open(`https://${input2.value}`);
+});
