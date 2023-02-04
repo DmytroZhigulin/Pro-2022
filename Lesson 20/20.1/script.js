5eff6ff25d319bc2747455436fc62b5319bca378
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-02-04 13:06:31 +0200

    Add files via upload

---
+++ Lesson 20/20.1/script.js
@@ -0,0 +1,7 @@
+// Використовуючи API https://jsonplaceholder.typicode.com/ зробити пошук поста за ід.
+// Ід має бути введений в інпут (валідація: ід від 1 до 100) Якщо знайдено пост, то вивести на сторінку блок з постом і зробити кнопку для отримання комкоментарів до посту.
+// Зробити завдання використовуючи проміси, перехопити помилки.
+const API = "https://jsonplaceholder.typicode.com"
+fetch(`${API}/posts/`)
+      .then(response => response.json())
+      .then(json => console.log(json))
