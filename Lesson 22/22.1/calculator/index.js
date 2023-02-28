a715db20d9e1ddee50e74e583e4494bcaea430ab
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-02-28 22:31:16 +0200

    Add files via upload

---
+++ Lesson 22/22.1/calculator/index.js
@@ -0,0 +1,11 @@
+const add = require('./add');
+const sub = require('./sub');
+const div = require('./div');
+const mult = require('./mult');
+
+module.exports = {
+  add,
+  sub,
+  div,
+  mult,
+};
