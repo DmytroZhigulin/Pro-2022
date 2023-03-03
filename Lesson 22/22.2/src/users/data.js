7d8548e8a8307d3b583a68a1f2237f7e829b356c
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-03-04 00:16:04 +0200

    Add files via upload

---
+++ Lesson 22/22.2/src/users/data.js
@@ -0,0 +1,24 @@
+export let users = [
+    {
+        userName: "John",
+        userSurname: "Rambo",
+        userAge: 38,
+        city: "Oclahoma",
+        email: "oclajohn92@gmail.com"
+    },
+    {
+        userName: "Alice",
+        userSurname: "Malkovich",
+        userAge: 20,
+        city: "Denver",
+        email: "jucilucy@gmail.com"
+    },
+    {
+        userName: "John",
+        userSurname: "McLain",
+        userAge: 65,
+        city: "New York",
+        email: "supercopNY@gmail.com"
+    }
+];
+
